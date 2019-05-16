dfd98d09920aa1b2821a6d15db1f9380639f0177
Bonggal Baringin Siahaan <bonggal.siahaan@gmail.com>
2019-05-16 08:29:28 +0700

    First commit

---
+++ OrderNumberGenerator/NumberGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderNumberGenerator
+{
+    public class NumberGenerator
+    {
+        public string GenerateNumber(string prefix)
+        {
+            Random generator = new Random();
+            String r = generator.Next(0, 1000000).ToString("D6");
+            string OrderNumber = String.Format("{0}-{1}{2}-{3}", prefix, DateTime.Now.Year, DateTime.Now.Month.ToString("D2"), r);
+            return OrderNumber;
+        }
+    }
+}
